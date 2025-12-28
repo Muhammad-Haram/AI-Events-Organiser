101c438bc16d1df12d36166de1ad461a11376a5f
Muhammad Haram <haramh643@gmail.com>
2025-12-28 05:27:40 -0800

    first commit

---
+++ app/layout.js
@@ -0,0 +1,50 @@
+import { ThemeProvider } from "@/components/theme-provider";
+import "./globals.css";
+import Header from "@/components/header";
+
+export const metadata = {
+  title: "Spott",
+  description: "Discover and create amazing events",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={'bg-linear-to-br from-gray-950 via-zinc-900 to-stone-900 text-white'}
+      >
+
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+
+          {/* header */}
+          <Header />
+
+          <main className="relative min-h-screen container mx-auto pt-40 md:pt-32">
+
+            {/* glow */}
+            <div className="pointer-events-none absolute inset-0 -z-10 overflow-hidden">
+              <div className="absolute top-0 left-1/4 w-80 h-80 bg-pink-600/20 rounded-full blur-3xl" />
+              <div className="absolute bottom-0 right-1/4 w-80 h-80 bg-orange-600/20 rounded-full blur-3xl" />
+            </div>
+
+            <div className="relative z-10 min-h-[70vh]">
+              {children}
+            </div>
+
+            {/* footer */}
+            <footer className="border-t border-gray-800/50 py-8 px-6 max-w-7xl mx-auto">
+              <div className="text-sm text-gray-400">Powered By Muhammad Haram</div>
+            </footer>
+
+          </main>
+        </ThemeProvider>
+
+      </body>
+    </html>
+  );
+}
---
+++ components/header.jsx
@@ -0,0 +1,34 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const Header = () => {
+    return (
+        <>
+            <nav className='fixed top-0 right-0 left-0 bg-background/80 backdrop-blur-xl z-20'>
+                <div className='max-w-7xl mx-auto px-6 py-4 flex items-center justify-between'>
+                    {/* logo */}
+
+                    <Link href={"/"} className='flex items-center' >
+                        <Image src="/spott.png" alt='Spott Logo' width={500} height={500} className='w-full h-11' priority />
+
+                        {/* pro badge */}
+
+                    </Link>
+
+                    {/* search & location - Desktop Only */}
+
+                    {/* Right Side Action */}
+                </div>
+
+                {/* search & location for mobile - below header */}
+
+            </nav>
+
+            {/* modals */}
+
+        </>
+    )
+}
+
+export default Header
